5147051802d65e17dc261b8ac5ef375955bf80b8
EC2 Default User <ec2-user@ip-172-16-37-59.us-east-2.compute.internal>
2020-04-14 18:44:38 +0000

    mp3 files updated for voice

--- src/processing_util.py
+++ src/processing_util.py
@@ -1,14 +1,13 @@
 class TranscriptionItem:
 
-    def __init__(self, item_dict):
+    def __init__(self, item_dict, offset=0):
         self.type = item_dict["type"]
         alternatives = item_dict["alternatives"][0]
         self.confidence = float(alternatives["confidence"])
         self.content = alternatives["content"]
         if self.is_word():
-            self.start_time = float(item_dict["start_time"]) * 1000 # milliseconds
-            self.end_time = float(item_dict["end_time"]) * 1000 # milliseconds
-
+            self.start_time = (float(item_dict["start_time"]) * 1000) + offset # milliseconds
+            self.end_time = (float(item_dict["end_time"]) * 1000) + offset # milliseconds
 
     def is_word(self):
         return self.type == "pronunciation"
