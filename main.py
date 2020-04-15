ae5eaf52cc705ded66f7191f5b68671b476c8187
Basil Wong <wong.bas@gmail.com>
2020-04-14 22:38:55 -0700

    redid main.py for dynamic timing

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 """
 Creates the final version of the mp3 using the output of the main.ipynb
 notebook.
+
+
 """
 
 
 import os
 import pydub
+import json
 from tqdm import tqdm
 
 # some_file.py
@@ -14,10 +17,12 @@ import sys
 sys.path.append('src')
 
 import audio_util
+import processing_util
 
 POLLY_OUTPUT_FOLDER = "polly-output/"
 BACKGROUND_FOLDER = "source-separation-output/background/"
 FINAL_OUTPUT_FOLDER = "final-output/"
+SONG_TRANSCRIPTION_PATH = "song-transcription/transcribed_song.json"
 
 # Generate Background
 background_mp3_files = [BACKGROUND_FOLDER + s for s in sorted(os.listdir(BACKGROUND_FOLDER))]
@@ -27,20 +32,24 @@ for fname in tqdm(background_mp3_files):
     background_mp3 += audio_util.interpret_polly_output_file(fname)
 background_mp3.export(FINAL_OUTPUT_FOLDER + "background.mp3", format="mp3")
 
-list_of_polly_output = [POLLY_OUTPUT_FOLDER + s for s in sorted(os.listdir(POLLY_OUTPUT_FOLDER))]
-vocal_mp3 = audio_util.interpret_polly_output_file(list_of_polly_output[0])
-list_of_polly_output.pop(0)
-for fname in tqdm(list_of_polly_output):
-    vocal_mp3 += audio_util.interpret_polly_output_file(fname)
+# Generate Vocals
+polly_output = sorted(os.listdir(POLLY_OUTPUT_FOLDER))
+song_transcription = json.load(open(SONG_TRANSCRIPTION_PATH, "r", encoding="utf-8"))
+
+vocal_mp3 = audio_util.get_silence(1)
+expected_start_time = 0
+
+for transcription_item, mp3_file in tqdm(zip(song_transcription, polly_output)):
+    if expected_start_time < transcription_item["start_time"]:
+        vocal_mp3 += audio_util.get_silence(transcription_item["start_time"] - expected_start_time)
+        expected_start_time = transcription_item["start_time"]
+
+    assert(mp3_file.startswith(transcription_item["index"]))
+    vocal_mp3 += audio_util.interpret_polly_output_file(POLLY_OUTPUT_FOLDER + mp3_file)
+    expected_start_time = transcription_item["end_time"]
+
 vocal_mp3.export(FINAL_OUTPUT_FOLDER + "vocals.mp3", format="mp3")
 
 final_audio = background_mp3.overlay(vocal_mp3)
 
 final_audio.export(FINAL_OUTPUT_FOLDER + "final_audio.mp3", format="mp3")
-    
-#     # Create pause if there is a larger than 1 second gap between words.
-#     if expected_start_time + add_pause_for_gaps_greater_than < transcribe_object.start_time:
-#         silence_dict["length"] = transcribe_object.start_time - expected_start_time
-#         with open(polly_output_folder + str(index).zfill(5) + ".json", 'w') as outfile:
-#             json.dump(silence_dict, outfile)
-#         expected_start_time = transcribe_object.start_time
